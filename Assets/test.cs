607c61ae2f918786dbb8c6e8bc449ae1f423482b
Peilin Li <lipeilin@gatech.edu>
2016-10-25 09:39:56 -0400

    initial commit

---
+++ Assets/test.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+    Vector3 oldPos;
+    [SerializeField]
+    GameObject marker;
+
+    Vector3 currentVelocity;
+    Vector3 currentAcceleration;
+
+    Vector3 oldVelocity;
+    Vector3 oldAcceleration;
+    [SerializeField]
+    float interval;
+    [SerializeField]
+    float noiseThreshold;
+
+    Vector3 deviceAccleration;
+    Vector3 calculatedTranslation;
+
+
+    
+
+	// Use this for initialization
+	void Start () {
+        oldAcceleration = FilterNoise(Input.acceleration, noiseThreshold);
+        oldAcceleration.z += 1f;
+        //StartCoroutine("MarkDevicePos");
+        StartCoroutine("MovePointer");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        deviceAccleration = Input.acceleration;
+        deviceAccleration.z += 1f;
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(currentAcceleration.x, 0, 0));
+        /*
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, Input.acceleration.y, 0));
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, 0, Input.acceleration.z+1f));
+        */
+    }
+
+    IEnumerator MarkDevicePos()
+    {
+        while (true)
+        {
+            GameObject mark = Instantiate(marker) as GameObject;
+            mark.transform.position = transform.position;
+
+            Destroy(mark, 10f);
+            yield return new WaitForSecondsRealtime(interval);
+        }
+    }
+    
+    IEnumerator MovePointer()
+    {
+        while (true)
+        {
+
+            currentAcceleration = Input.acceleration;
+            currentAcceleration.z += 1f;
+            currentAcceleration = FilterNoise(currentAcceleration, noiseThreshold);
+            currentVelocity = oldVelocity + oldAcceleration * interval;
+
+            transform.position += GetTranslation();
+            oldAcceleration = currentAcceleration;
+            oldVelocity = currentVelocity;
+            
+            yield return new WaitForSecondsRealtime(interval);
+        }
+    }
+
+    Vector3 GetTranslation()
+    {
+
+        Vector3 jerk = (currentAcceleration - oldAcceleration) / interval;
+        //Vector3 jerk = Vector3.zero;
+        if (jerk.magnitude == 0)
+        {
+            return Vector3.zero;
+        }
+
+        Vector3 translation = oldVelocity * interval + 0.5f * oldAcceleration * Mathf.Pow(interval, 2f) + (1f/6f) * jerk * Mathf.Pow(interval, 3f);
+        calculatedTranslation = translation;
+        return new Vector3(translation.x, 0, 0);
+    }
+
+    Vector3 FilterNoise(Vector3 vec, float threshold)
+    {
+        vec.x = Mathf.Abs(vec.x) > threshold ? vec.x : 0;
+        vec.y = Mathf.Abs(vec.y) > threshold ? vec.y : 0;
+        vec.z = Mathf.Abs(vec.z) > threshold ? vec.z : 0;
+        return vec;
+    }
+
+}
